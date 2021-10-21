39432460b8a02ddeadb37b767d546f12539fe8a9
MerisStupar <merisstupar11@gmail.com>
2021-10-21 10:53:45 +0200

    Load function for meme added

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,23 @@
 const imageFileInput = document.querySelector('#imageFileInput');
 const topTextInput = document.querySelector('#inputTopText');
 const bottomTextInput = document.querySelector('#inputBottomText');
-const cavnas = document.querySelector('#meme');
+const canvas = document.querySelector('#meme');
 
 let image;
 
 imageFileInput.addEventListener("change", ()=>{     //for change image
     const imageDataUrl = URL.createObjectURL(imageFileInput.files[0]);
     
-    console.log(imageDataUrl);
+    image = new Image();
+    image.src = imageDataUrl;
 
-});
+    image.addEventListener("load", ()=>{
+        updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
+    }, { once:true });
 
-function updateMemeCanvas(canvas, image, topTextText, bottomText){
+});          
+
+function updateMemeCanvas(canvas, image, topText, bottomText){
     console.log(canvas);
     console.log(image);
     console.log(topText);
