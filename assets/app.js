dce8a6bc91ccc5fbb47b9af6162877f97485d8f2
MerisStupar <merisstupar11@gmail.com>
2021-10-21 13:08:29 +0200

    Added prepare text, top & bottom text

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,50 @@
-const imageFileInput = document.querySelector('#imageFileInput');
-const topTextInput = document.querySelector('#inputTopText');
-const bottomTextInput = document.querySelector('#inputBottomText');
-const canvas = document.querySelector('#meme');
+const imageFileInput = document.querySelector("#imageFileInput");
+const canvas = document.querySelector("#meme");
+const topTextInput = document.querySelector("#topTextInput");
+const bottomTextInput = document.querySelector("#bottomTextInput");
 
 let image;
 
-imageFileInput.addEventListener("change", ()=>{     //for change image
-    const imageDataUrl = URL.createObjectURL(imageFileInput.files[0]);
-    
-    image = new Image();
-    image.src = imageDataUrl;
+imageFileInput.addEventListener("change", (e) => {          //for change image
+  const imageDataUrl = URL.createObjectURL(e.target.files[0]);
 
-    image.addEventListener("load", ()=>{
-        updateMemeCanvas(canvas, image, topTextInput.value, bottomTextInput.value);
-    }, { once:true });
+  image = new Image();
+  image.src = imageDataUrl;
 
-});          
+  image.addEventListener( "load",() => {
+      updateMemeCanvas(canvas,image,topTextInput.value,bottomTextInput.value)
+    },{ once: true }
+);
+});
 
-function updateMemeCanvas(canvas, image, topText, bottomText){
-    const ctx = canvas.getContext ("2d");
-    const width = image.width;
-    const height = image.height;
 
-    // For update canvas background
-    canvas.widht = width;
-    canvas.height = height;
-    ctx.drawImage(image, 0, 0, 350, 400);   //image, position (0), postinon (0) & widht/height
+function updateMemeCanvas(canvas, image, topText, bottomText) {
+  const ctx = canvas.getContext("2d");
+  const width = image.width;
+  const height = image.height;
+  const fontSize = Math.floor(width / 10);
+  const yOffset = height / 25;
 
+  // Update canvas background
+  canvas.width = width;
+  canvas.height = height;
+  ctx.drawImage(image, 0, 0);    //image, position (0), postinon (0)
+
+  // Prepare text
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = Math.floor(fontSize / 4);
+  ctx.fillStyle = "white";
+  ctx.textAlign = "center";
+  ctx.lineJoin = "round";
+  ctx.font = `${fontSize}px sans-serif`;
+
+  // Add top text
+  ctx.textBaseline = "top";
+  ctx.strokeText(topText, width / 2, yOffset);
+  ctx.fillText(topText, width / 2, yOffset);
+
+  // Add bottom text
+  ctx.textBaseline = "bottom";
+  ctx.strokeText(bottomText, width / 2, height - yOffset);
+  ctx.fillText(bottomText, width / 2, height - yOffset);
 }
